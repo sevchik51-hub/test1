8c55009cdca7afb88d4d69a43072dd276368c40d
Seva <sevchik51@yandex.ru>
2026-09-05 19:34:08 +0300

    bug fix

--- bbsort.cpp
+++ bbsort.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void bubbleSort(vector<int>& arr) {
-    int n = arr.size();
+    int n = (int)arr.size();
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
